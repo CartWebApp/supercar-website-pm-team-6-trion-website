94edc5eb4976c4cebea74bd072ed50195a205985
AAflip <jedidjm2008@gmail.com>
2024-12-12 15:49:05 -0800

    hgabsidlhcjkNAZLSFhslkjdn><DCYFHlksdhfoikuhgfskjH IM SURE WE GOT THIS

--- carting.js
+++ carting.js
@@ -1,4 +1,3 @@
-const
 // set the name of your shop here
 shopID = 'trionShop',
 // match the following attributes to the classes on your products
@@ -29,6 +28,19 @@ constructor(name, desc, price, imgSrc, qty = 1) {
     this.imgSrc = imgSrc;
     this.qty = qty;
 }
+}
+
+window.onresize = function inExAngle() {
+    let height = window.screen.height;
+    let width = window.screen.width;
+    let temp = Math.atan(height/width);
+    let answer = 90 - temp;
+    let theElements=document.getElementsByClassName("option");
+    let theyElements = document.getElementsByClassName("rotate");
+    theElements[0].style.transform = `skewX(${-answer}deg)`;
+    theElements[1].style.transform = `skewX(${-answer}deg)`;
+    theyElements[0].style.transform = `skewX(${answer}deg)`;
+    theyElements[0].style.transform = `skewX(${answer}deg)`;
 }
 
 function addToCart(e) {
